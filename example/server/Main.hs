27e7cbe3ab4d6bedde99fc45bd24b2710cec118b
Athan Clark <athan.clark@gmail.com>
2018-03-20 12:48:37 -0600

    working, actually kills server thread

--- example/server/Main.hs
+++ example/server/Main.hs
@@ -19,15 +19,20 @@ import Network.Wai.Handler.Warp (run)
 import Network.Wai.Middleware.ContentType.Text (textOnly)
 import Network.HTTP.Types (status404)
 
-import Data.Singleton.Class (Extractable)
+import Data.Singleton.Class (Extractable (..))
+import Control.Monad (forever)
 import Control.Monad.Trans (MonadTrans (..))
 import Control.Monad.IO.Class (MonadIO (..))
 import Control.Monad.Catch (MonadCatch)
 import Control.Monad.Trans.Control (MonadBaseControl)
 import qualified Control.Monad.Trans.Control.Aligned as Aligned
+import Control.Concurrent (threadDelay)
+import Control.Concurrent.Async (async, link)
 
 
 server :: MonadIO m
+       => Aligned.MonadBaseControl IO m stM
+       => Extractable stM
        => Server m InitIn InitOut DeltaIn DeltaOut
 server InitIn = do
   liftIO $ putStrLn "  ## Init"
@@ -37,11 +42,16 @@ server InitIn = do
         pure ServerReturn
           { serverInitOut = InitOut
           , serverOnOpen = \ServerArgs{serverDeltaReject,serverSendCurrent} -> do
-              liftIO $ putStrLn "  ## Sending DeltaOut onOpen"
-              serverSendCurrent DeltaOut
+              Aligned.liftBaseWith $ \runInBase -> do
+                let runM x = runSingleton <$> runInBase x
+                thread <- async $ forever $ do
+                  putStrLn "  ## Sending DeltaOut onOpen"
+                  runM (serverSendCurrent DeltaOut)
+                  threadDelay $ (10^6) * 2
+                pure (Just thread)
           , serverOnReceive = \ServerArgs{serverDeltaReject,serverSendCurrent} DeltaIn -> do
               liftIO $ putStrLn "  ## Received DeltaIn and Rejecting"
-              serverDeltaReject
+              -- serverDeltaReject
           }
     , serverOnUnsubscribe = do
         liftIO $ putStrLn "  ## Unsubscribed"
